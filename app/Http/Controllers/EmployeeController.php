af87a0d3a3fb9d4a20e823317e96074164bb08ce
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-13 14:23:30 +0700

    Fitur Employees - Handle Create

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use Illuminate\Http\Request;
 use App\Models\Employee;
+use App\Models\Role;
 
 class EmployeeController extends Controller
 {
@@ -11,5 +13,30 @@ class EmployeeController extends Controller
     public function index(){
         $employees = Employee::all();
         return view("employees.index",compact("employees"));
+    }
+
+    public function create(){
+        $departments = Department::all();
+        $roles = Role::all();
+        return view("employees.create", compact("departments","roles"));
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'fullname' => 'required|string|max:255',
+            'email' => 'email|required',
+            'phone_number' => 'string|required|max:15',
+            'address' => 'nullable|required',
+            'birth_date' => 'required|date',
+            'hire_date' => 'required|date',
+            'department_id' => 'required',
+            'role_id' => 'required',
+            'status' => 'string|required',
+            'salary' => 'numeric|required'
+        ]);
+
+        Employee::create($request->all());
+
+        return redirect()->route('employees.index')->with('success','Employee created successfully');
     }
 }
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -66,13 +66,13 @@
                             <td>{{$employee->role->title}}</td>
                             <td>
                                 @if($employee->status == 'active')
-                                    <span class="text-success">{{$employee->status}}</span>
+                                    <span class="text-success">{{ucfirst($employee->status)}}</span>
                                 @else
-                                    <span class="text-warning">{{$employee->status}}</span>
+                                    <span class="text-warning">{{ucfirst($employee->status)}}</span>
                                 @endif
                                 
                             </td>
-                            <td>{{$employee->salary}}</td>
+                            <td>{{number_format($employee->salary)}}</td>
 
                             <td>
                                 <a href="{{ route('employees.show', $employee->id) }}" target="_blank" class="btn btn-info btn-sm">View</a>
