ecdcf0270b937885a22f26c1c33ff86b7820b0df
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-13 10:51:52 +0700

    Fitur Tasks - Handle Delete

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -81,7 +81,12 @@
                                 @endif
 
                                 <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                <a href="" class="btn btn-danger btn-sm">Delete</a>
+
+                                <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display: inline">
+                                    @csrf
+                                    @method ('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
                             </td>
                         </tr>
 
