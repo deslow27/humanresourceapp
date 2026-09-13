c3b46dbae79d2385419e63aad214cbb9f8287e4c
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-13 23:52:32 +0700

    Fitur Roles - Create

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -13,4 +13,21 @@ class RoleController extends Controller
         $roles = Role::all();
         return view("roles.index", compact("roles"));
     }
+
+    public function create()
+    {
+        return view("roles.create");
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+        'title' => 'required|string|max:255',
+        'description' => 'nullable'
+        ]);
+
+        Role::create($request->all());
+
+        return redirect()->route('roles.index')->with('success','Role created successfully');
+    }
 }
