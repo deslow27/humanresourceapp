e1d12b01cce87ae65c5de15ef8e52cb3da621d3d
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-13 15:27:39 +0700

    Fitur Employees - Delete Data

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -73,4 +73,11 @@ class EmployeeController extends Controller
 
         return redirect()->route('employees.index')->with('success','Employee update successfully');
     }
+
+    public function destroy($id){
+        $employee = Employee::findOrFail($id);
+        $employee->delete();
+
+        return redirect()->route('employees.index')->with('success','Employee deleted successfully');
+    }
 }
