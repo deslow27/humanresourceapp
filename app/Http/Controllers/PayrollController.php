36f06f214bc0ee721d10f94309406df5463f2f86
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-14 08:40:31 +0700

    Fitur Payrolls - Create

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use App\Models\Payroll;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,28 @@ class PayrollController extends Controller
     public function index(){
         $payrolls = Payroll::all();
         return view("payrolls.index", compact("payrolls"));
+    }
+
+    public function create(){
+        $employees = Employee::all();
+        return view("payrolls.create", compact("employees"));
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            "employee_id"=> "required",
+            "salary"=> "required|numeric",
+            "bonuses"=> "required|numeric",
+            "deductions"=> "required|numeric",
+            "pay_date"=> "required|date",
+        ]);
+
+        $netSalary = $request->input("salary") - $request->input("deductions") + $request->input("bonuses");
+
+        $request->merge(['net_salary' => $netSalary]);
+
+        Payroll::create($request->all());
+
+        return redirect()->route('payrolls.index')->with("success","Payroll created successfully");
     }
 }
