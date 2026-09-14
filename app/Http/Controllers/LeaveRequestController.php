108ad94c3a07b3118f86ea122bf3ee230f94e17b
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-14 10:04:25 +0700

    Fitur Leave Requests - Update

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -37,4 +37,23 @@ class LeaveRequestController extends Controller
         return redirect()->route('leave-requests.index')->with('success', 'Leave request created successfully.');
     }
 
+    public function edit(LeaveRequest $leaveRequest) {
+        $employees = Employee::all();
+
+        return view('leave-requests.edit', compact('leaveRequest', 'employees'));
+    }
+
+    public function update(Request $request, LeaveRequest $leaveRequest) {
+    $request->validate([
+        'employee_id' => 'required',
+        'leave_type' => 'required|string',
+        'start_date' => 'required|date',
+        'end_date' => 'required|date'
+    ]);
+
+    $leaveRequest->update($request->all());
+
+    return redirect()->route('leave-requests.index')->with('success', 'Leave request updated successfully.');
+}
+
 }
