6bdafa74f9be28dffae76bef050d8345f332b0b4
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-14 07:49:43 +0700

    Fitur Payrolls - Index

---
+++ app/Http/Controllers/PayrollController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payroll;
+use Illuminate\Http\Request;
+
+class PayrollController extends Controller
+{
+    //
+    public function index(){
+        $payrolls = Payroll::all();
+        return view("payrolls.index", compact("payrolls"));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\PresenceController;
+use App\Http\Controllers\PayrollController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,7 @@ Route::resource('/employees', EmployeeController::class);
 Route::resource('/departments', DepartmentController::class);
 Route::resource('/roles', RoleController::class);
 Route::resource('/presences', PresenceController::class);
+Route::resource('/payrolls', PayrollController::class);
 
 Route::resource('/tasks', TaskController::class);
 Route::get('tasks/done/{id}', [TaskController::class, 'done'])->name('tasks.done');
